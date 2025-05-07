9467c17685600b991de4a5a5b4a95ca972b7cf5a
Lee Howard <leegibsonhoward@gmail.com>
2025-05-07 00:28:50 -0700

    improve theme switcher
    - use localstorage for saving selected theme
    
    - fallback to system preference if no saved theme

--- modules/theme-switcher.mjs
+++ modules/theme-switcher.mjs
@@ -1,15 +1,37 @@
 import { DEBUG } from "../main.mjs";
 
-export function setupThemeSwitcher() {
-    const toggleBtn = document.getElementById("theme-toggle");
-    toggleBtn.addEventListener("click", () => {
-        const html = document.documentElement;
-        const current = html.getAttribute("data-theme");
-        const next = current === "light" ? "dark" : "light";
-        html.setAttribute("data-theme", next);
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+}
 
-        if (DEBUG) {
-            console.debug(`Theme: ${next}`);
-        }
-    });
+// On page load, apply saved theme or fallback to system preference
+function loadTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        setTheme(savedTheme);
+    } else {
+        const prefersDark = window.matchMedia(
+            "(prefers-color-scheme: dark)"
+        ).matches;
+        setTheme(prefersDark ? "dark" : "light");
+    }
+}
+
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    if (DEBUG) {
+        console.debug(`Theme: ${newTheme}`);
+    }
+}
+
+export function setupThemeSwitcher() {
+    // load saved theme from local storage
+    loadTheme();
+
+    // wait on button click.
+    const toggleBtn = document.querySelector(".theme-toggle");
+    toggleBtn.addEventListener("click", toggleTheme);
 }
